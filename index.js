e29e017dbadf38a5f6812a970de80bd085f629db
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-10-18 12:30:26 -0400

    Log Links

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-let myLeads = []
+let myLinks = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const removeBtn = document.getElementById("remove-btn")
 
+const linksFromStorage = JSON.parse(localStorage.getItem("myLinks"))
+
+if (linksFromStorage) {
+    myLinks = linksFromStorage
+    render(myLinks)
+}
+
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    myLinks.push(inputEl.value)
     inputEl.value = ""
-    renderLeads()
+    localStorage.setItem("myLinks", JSON.stringify(myLinks))
+    render(myLinks)
 })
 
-removeBtn.addEventListener("click", function() {
-    removeLeads()
+removeBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLinks = [];
+   render(myLinks) 
+   ulEl.innerHTML = ''
     
 })
 
-function renderLeads() {
+function render(links) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < links.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${links[i]}'>
+                    ${links[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems  
-}
-
-function removeLeads(){
-    myLeads = [];
-    ulEl.innerHTML = '' 
 }
 
 
